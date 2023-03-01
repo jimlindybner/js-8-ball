f7f46317e60dee1c9824737695694b7c60c7a3b6
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-03-01 10:24:13 -0500

    html, css, js added. while loops introduced to check for error and repeat game if player desires.

---
+++ script.js
@@ -0,0 +1,78 @@
+let userQuestion = "";
+let eightBall = "";
+let randomNum;
+let tryAgain = true;
+
+while (tryAgain) {
+  randomNum = randomNum = Math.floor(Math.random() * 20);
+  userQuestion = prompt(`What is your question?`);
+
+  while (!userQuestion) {
+    userQuestion = prompt(`Please ask a real question.`);
+  }
+  switch (randomNum) {
+    case 0:
+      eightBall = "It is certain.";
+      break;
+    case 1:
+      eightBall = "It is decidedly so.";
+      break;
+    case 2:
+      eightBall = "Without a doubt.";
+      break;
+    case 3:
+      eightBall = "Yes definitely.";
+      break;
+    case 4:
+      eightBall = "You may rely on it.";
+      break;
+    case 5:
+      eightBall = "As I see it, yes.";
+      break;
+    case 6:
+      eightBall = "Most likely.";
+      break;
+    case 7:
+      eightBall = "Outlook good.";
+      break;
+    case 8:
+      eightBall = "Yes.";
+      break;
+    case 9:
+      eightBall = "Signs point to yes.";
+      break;
+    case 10:
+      eightBall = "Reply hazy, try again.";
+      break;
+    case 11:
+      eightBall = "Ask again later.";
+      break;
+    case 12:
+      eightBall = "Better not tell you now.";
+      break;
+    case 13:
+      eightBall = "Cannot predict now.";
+      break;
+    case 14:
+      eightBall = "Concentrate and ask again.";
+      break;
+    case 15:
+      eightBall = "Don't count on it.";
+      break;
+    case 16:
+      eightBall = "My reply is no.";
+      break;
+    case 17:
+      eightBall = "My sources say no.";
+      break;
+    case 18:
+      eightBall = "Outlook not so good.";
+      break;
+    default:
+      eightBall = "Very doubtful.";
+      break;
+  }
+
+  alert(eightBall);
+  tryAgain = confirm(`Try again?`);
+}
