a6e9c4323df479a70065742fc73ae0286ce1866c
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-03-06 13:17:53 -0500

    prints question and answer to DOM

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
-// variables
-let userQuestion = "";
-let eightBall = "";
-let randomNum;
-let tryAgain = true;
-const answers = [
-  "It is certain.",
-  "It is decidedly so.",
-  "Without a doubt.",
-  "Yes definitely.",
-  "You may rely on it.",
-  "As I see it, yes.",
-  "Most likely.",
-  "Outlook good.",
-  "Yes.",
-  "Signs point to yes.",
-  "Reply hazy, try again.",
-  "Ask again later.",
-  "Better not tell you now.",
-  "Cannot predict now.",
-  "Concentrate and ask again.",
-  "Don't count on it.",
-  "My reply is no.",
-  "My sources say no.",
-  "Outlook not so good.",
-  "Very doubtful.",
-];
+window.onload = pageReady;
 
-// repeat as long as tryAgain is truthy
-while (tryAgain) {
-  // generate randomNum value
-  randomNum = Math.floor(Math.random() * answers.length);
-  // ask for userQuestion
-  userQuestion = prompt(`Please ask a yes or no question?`);
+function pageReady() {
+  // variables
+  let userQuestion = "";
+  let eightBall = "";
+  let randomNum;
+  let tryAgain = true;
+  const answers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+  ];
 
-  // validating question - repeat while userQuestion is falsy
-  while (!userQuestion) {
-    userQuestion = prompt(`Please ask a real question.`);
+  // repeat as long as tryAgain is truthy
+  while (tryAgain) {
+    // generate randomNum value
+    randomNum = Math.floor(Math.random() * answers.length);
+    // ask for userQuestion
+    userQuestion = prompt(`Please ask a yes or no question?`);
+
+    // validating question - repeat while userQuestion is falsy
+    while (!userQuestion) {
+      userQuestion = prompt(`Please ask a real question.`);
+    }
+
+    // generate eightBall response based on answers arr and randomNum
+    eightBall = answers[randomNum];
+
+    // return eightBall response
+    document.getElementById(
+      "eightBall"
+    ).innerHTML = `Your question: "${userQuestion}" <br><br> The answer: "${eightBall}"`;
+
+    // ask if user wants to try again
+    tryAgain = confirm(`Do you want to ask another question?`);
   }
-
-  // generate eightBall response based on answers arr and randomNum
-  eightBall = answers[randomNum];
-
-  // return eightBall response
-  alert(eightBall);
-
-  // ask if user wants to try again
-  tryAgain = confirm(`Do you want to ask another question?`);
 }
